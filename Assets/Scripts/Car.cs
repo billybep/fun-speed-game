989e89f79173565af2701d44b92cd7acc2a77b3b
billybep <billy.pesoth@gmail.com>
2023-02-07 12:28:32 +0800

    renew asset + add block area

--- Assets/Scripts/Car.cs
+++ Assets/Scripts/Car.cs
@@ -18,6 +18,8 @@ public class Car : MonoBehaviour
         transform.Rotate(0f, steerValue * _turnSpeed * Time.deltaTime, 0f);
 
         transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        checkPosition();
     }
 
     private void OnTriggerEnter(Collider other)
@@ -31,5 +33,20 @@ public class Car : MonoBehaviour
     public void Steer(int value)
     {
         steerValue = value;
+    }
+
+    void checkPosition()
+    {
+        if (transform.position.y <= -10.0f || transform.position.y >= 5.0f )
+        {
+            SceneManager.LoadScene("Scene_MainMenu");
+            Debug.Log("Gameover");
+        }
+
+        if (transform.position.x >= 70.0f || transform.position.x <= -155.0f || transform.position.z >= 105.0f || transform.position.z <= -75.0f )
+        {
+            SceneManager.LoadScene("Scene_MainMenu");
+            Debug.Log("Gameover");
+        }
     }
 }
